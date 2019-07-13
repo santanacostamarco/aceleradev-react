e90f872f7f17e70462054c2a75e3894052e37211
Marco Santana <santannacostamarco@gmail.com>
2019-07-13 16:35:55 -0300

    spread

--- semana_1/ex3.js
+++ semana_1/ex3.js
@@ -1,11 +1,11 @@
 // Modifique a função sum para que ela use o operador rest e funcione com qualquer numero de parametros
 const sum = (function() {
  "use strict";
- return function sum() {
-   const args = [ ...arguments ];
+ return function sum(...arr) {
+   const args = arr;
    return args.reduce((a, b) => a + b, 0);
  };
 })();
 console.log(
-    sum(1, 2, 3, 4)
+    sum(1, 2, 3, 4, 10)
     ); // 6
